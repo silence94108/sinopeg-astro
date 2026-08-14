53c8161f401533f3682a5bc9b031e9352e4235b6
畅睿 <979922105@qq.com>
2026-08-14 15:43:48 +0800

    添加 Greeting 组件，更新首页以动态显示欢迎语；更新配置以支持 Preact

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import preact from "@astrojs/preact";
+
 // https://astro.build/config
 export default defineConfig({
-  site: "https://incredible-sprite-dc185e.netlify.app"
+  site: "https://incredible-sprite-dc185e.netlify.app",
+  integrations: [preact()]
 });
