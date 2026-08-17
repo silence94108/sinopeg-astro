f0226b82d0d1cd396714d6f17d54f223f77a760b
畅睿 <979922105@qq.com>
2026-08-17 14:42:54 +0800

    优化项目结构 去掉多余文件

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,5 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import preact from "@astrojs/preact";
-
 // https://astro.build/config
-export default defineConfig({
-  site: "https://incredible-sprite-dc185e.netlify.app",
-  integrations: [preact()]
-});
+export default defineConfig({});
