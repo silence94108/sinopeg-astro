6dda743e1061b487521e9b467761e235330faf6e
畅睿 <979922105@qq.com>
2026-09-03 15:52:08 +0800

    更新打包输出目录配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
+  outDir: './dist',  // 打包输出目录，可以改成任意路径，如 './build' 或 '../sinopeg-output'
   vite: {
     cacheDir: '.astro/vite-cache'
   }
